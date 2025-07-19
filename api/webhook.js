8b07211780ec8a9851c8c1f98caa7debc2ba5b6e
Krish Bhagat <sonakb666@gmail.com>
2025-07-20 00:33:32 +0530

    deployment fix 2

--- api/webhook.js
+++ api/webhook.js
@@ -1,22 +1,20 @@
 export default function handler(req, res) {
+    const VERIFY_TOKEN = 'krish_custom_token';
+  
     if (req.method === 'GET') {
-      const VERIFY_TOKEN = 'krish_custom_token';
       const mode = req.query['hub.mode'];
       const token = req.query['hub.verify_token'];
       const challenge = req.query['hub.challenge'];
   
-      if (mode && token) {
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-          console.log('WEBHOOK_VERIFIED');
-          res.status(200).send(challenge);
-        } else {
-          res.sendStatus(403);
-        }
+      if (mode && token && mode === 'subscribe' && token === VERIFY_TOKEN) {
+        res.status(200).send(challenge);
+      } else {
+        res.sendStatus(403);
       }
     } else if (req.method === 'POST') {
-      console.log('Incoming webhook event:', req.body);
+      console.log('Webhook event received:', JSON.stringify(req.body, null, 2));
       res.sendStatus(200);
     } else {
-      res.status(405).end(); // Method Not Allowed
+      res.status(405).end();
     }
   }
